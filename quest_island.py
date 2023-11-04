25f46c54e04ded01b3c93ca1658d633c1fc266b1
park102 <BJohn316@outlook.com>
2023-11-04 11:36:35 -0400

    made tab indenting to space indenting stats update every cycle.

--- quest_island.py
+++ quest_island.py
@@ -6,12 +6,12 @@ import database
 import tp
 
 def intinput(question=""):
-	answer = input(question)
-	try:
-		int(answer)
-		return int(answer)
-	except:
-		pass
+    answer = input(question)
+    try:
+        int(answer)
+        return int(answer)
+    except:
+        pass
 
 print("""\n\n\n!!!!!!!!!!WARNING!!!!!!!!!!
 THIS IS A DEMO!\n\n\n""")
@@ -21,54 +21,53 @@ user = database.choose_user()
 db = database.Db(user)
 data = db.get_file()
 post = tp.Trading_Post(user)
-
-stats = """User: {user}
+def main():
+    while True:
+        stats = """User: {user}
 Money: ${money}
 Job:   {job}""".format(user=db.user, money=data['money'], job=('None' if not data['job'] else data['job']))
-
-def main():
-	while True:
-		print("\nStats:")
-		print(stats)
-		print("=============================")
-		print("Options:")
-		if not data['job']:
-			print("[0] Beg for Money")
-		else:
-			print("[0] Work")
-		print("[1] Trading Post")
-		if not data['job']:
-			print("[2] Get a job")
-		else:
-			work()
-		answer = intinput()
-		if answer == 1:
-			post.main()
-		if answer == 2 and not data['job']:
-			get_job()
-		db.save_file(data)
+        print("\nStats:")
+        print(stats)
+        print("=============================")
+        print("Options:")
+        if not data['job']:
+            print("[0] Beg for Money")
+        else:
+            print("[0] Work")
+        print("[1] Trading Post")
+        if not data['job']:
+            print("[2] Get a job")
+        else:
+            work()
+        answer = intinput()
+        if answer == 1:
+            post.main()
+        if answer == 2 and not data['job']:
+            get_job()
+        db.save_file(data)
 
 def beg():
-	print("Sorry! This feature is unavailiable on the demo version.")
-	return
+    print("Sorry! This feature is unavailiable on the demo version.")
+    return
 
 def work():
-	# Blacksmiths average $15-22/hour
-	# Carpenter average   $16-36/hour
-	# 
-	earned = 0
-	if data['job'] == "Blacksmith":
-		wage = random.randint(15, 22)
-	if data['job'] == "Carpenter":
-		wage = random.randint(16, 36)
-	earned = 8 * wage
-	data['money'] += earned
-	print(f"You worked as a {data['job']} and got {earned} dollars. You now go home.")
-	return
+    # Blacksmiths average $15-22/hour
+    # Carpenter average   $16-36/hour
+    # 
+    earned = 0
+    if data['job'] == "Blacksmith":
+        wage = random.randint(15, 22)
+    if data['job'] == "Carpenter":
+        wage = random.randint(16, 36)
+    earned = 8 * wage
+    data['money'] += earned
+    print(f"You worked as a {data['job']} and got {earned} dollars. You now go home.")
+    return
 
 def get_job():
-	print("Sorry! This feature is unavailiable on the demo version.")
-	return
+    print("Sorry! This feature is unavailiable on the demo version.")
+    return
 
 if __name__=='__main__':
-	main()
+    main()
+

32277b28e87034186fd6d7b5684fe646125b6f9c
Dalton Knapp <126925044+PortMaster1@users.noreply.github.com>
2023-11-07 22:16:12 -0500

    Update tp.py
    Added user print marks for important interactions.

--- tp.py
+++ tp.py
@@ -41,26 +41,29 @@ class Trading_Post:
         answer = intinput()
         if answer == 0:
             sure = intinput("Buy Bread?\n[0] Yes\n[1] No\n")
-            if sure == 0:
-                amount = intinput("How many loaves? (Number)  ")
-                cost = amount * 5
-                if data['money'] >= cost:
-                    db.add_item(f"bread x{amount}")
-                    data['money'] -= cost
-                    print(f"You bought {amount} loaves of bread for {cost} dollars.")
-                else:
-                    print("You don't have enough money!")
-                if answer == 1:
-                    sure = intinput("Buy 1 Cake?\n[0] Yes\n[1] No")
-                    if sure == 0:
-                        cost = 10
-                        if data['money'] >= cost:
-                            db.add_item(f"Cake x1")
-                            data['money'] -= cost
-                            print("You bought 1 cake for 10 dollars.")
-                        else:
-                            print("You don't have enough money!")
-            return	
+            if sure == 1:
+                return
+            amount = intinput("How many loaves? (Number)  ")
+            cost = amount * 5
+            if data['money'] >= cost:
+                db.add_item(f"bread x{amount}")
+                print(f"* Acquired {amount}x Bread")
+                data['money'] -= cost
+            else:
+                print("You don't have enough money!")
+        if answer == 1:
+            sure = intinput("Buy 1 Cake?\n[0] Yes\n[1] No")
+            if sure == 1;
+                return
+            cost = 10
+            if data['money'] >= cost:
+                db.add_item(f"Cake x1")
+                print("* Acquired 1x Cake")
+                data['money'] -= cost
+            else:
+                print("You don't have enough money!")
+            return
+
     def weapons(self):
         db = self.db
         data = self.data
@@ -75,8 +78,8 @@ class Trading_Post:
             cost = 30
             if data["money"] >= cost:
                 db.add_item("Club x1")
+                print("* Acquired 1x Club")
                 data["money"] -= cost
-                print("You bought a club")
             else:
                 print("You don't have enough money!")
         if answer == 1:
@@ -86,8 +89,8 @@ class Trading_Post:
             cost = 50
             if data["money"] >= cost:
                 db.add_item("Axe x1")
+                print("* Acquired 1x Axe")
                 data["money"] -= cost
-                print("You bought an axe")
             else:
                 print("You don't have enough money!")
         return
